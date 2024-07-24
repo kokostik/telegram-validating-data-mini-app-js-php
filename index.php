8321f1a64c1a59cd43449f7531add46397412806
Konstantin <84973081+kokostik@users.noreply.github.com>
2024-07-24 20:10:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Validate Telegram user</title>
+    <script src="https://telegram.org/js/telegram-web-app.js" defer></script>
+    <script src="core/validation.js?v=<?php echo time(); ?>" defer></script>
+</head>
+<body style="background-color: white;">
+    <div id="validation" style="text-align: center;">validation...</div>
+</body>
+</html>
